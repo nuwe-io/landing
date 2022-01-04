fffe40b0e0fefc914bb95435a795d1ccdda3695d
Edgar <edgargc.upc@gmail.com>
2022-01-04 13:27:27 +0100

    base config

---
+++ next.config.js
@@ -0,0 +1,80 @@
+const { BUSINESS_URL, DEV_URL, BACKOFFICE_URL, BLOG_URL } = process.env;
+
+module.exports = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/business",
+          destination: `${BUSINESS_URL}/business`,
+        },
+        {
+          source: "/business/:path*",
+          destination: `${BUSINESS_URL}/business/:path*`,
+        },
+        {
+          source: "/dev",
+          destination: `${DEV_URL}/dev`,
+        },
+        {
+          source: "/dev/:path*",
+          destination: `${DEV_URL}/dev/:path*`,
+        },
+        {
+          source: "/backoffice",
+          destination: `${BACKOFFICE_URL}/backoffice`,
+        },
+        {
+          source: "/backoffice/:path*",
+          destination: `${BACKOFFICE_URL}/backoffice/:path*`,
+        },
+        {
+          source: "/blog",
+          destination: `${BLOG_URL}/blog`,
+        },
+        {
+          source: "/blog/:path*",
+          destination: `${BLOG_URL}/blog/:path*`,
+        },
+      ],
+      fallback: [
+        {
+          source: "/business",
+          destination: `${BUSINESS_URL}/business`,
+        },
+        {
+          source: "/business/:path*",
+          destination: `${BUSINESS_URL}/business/:path*`,
+        },
+        {
+          source: "/dev",
+          destination: `${DEV_URL}/dev`,
+        },
+        {
+          source: "/dev/:path*",
+          destination: `${DEV_URL}/dev/:path*`,
+        },
+        {
+          source: "/backoffice",
+          destination: `${BACKOFFICE_URL}/backoffice`,
+        },
+        {
+          source: "/backoffice/:path*",
+          destination: `${BACKOFFICE_URL}/backoffice/:path*`,
+        },
+        {
+          source: "/blog",
+          destination: `${BLOG_URL}/blog`,
+        },
+        {
+          source: "/blog/:path*",
+          destination: `${BLOG_URL}/blog/:path*`,
+        },
+      ],
+    };
+  },
+};
--- src/components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-    return (  
-        <h1> THIS IS A FOOTER</h1>
-    );
-}
-
-export default Footer;
