937913d7b83d14d3e9f4d2905f6644862c98b0fa
Edgar <edgargc.upc@gmail.com>
2022-03-29 12:13:43 +0200

    press and privacy views

--- dist/pages.dev.js
+++ dist/pages.dev.js
@@ -12,4 +12,19 @@ module.exports = [{
 }, {
   source: "/developers",
   destination: "/developers.html"
+}, {
+  source: "/press",
+  destination: "/press-release.html"
+}, {
+  source: "/privacy",
+  destination: "/privacy-policy.html"
+}, {
+  source: "/terms",
+  destination: "/terms-of-use.html"
+}, {
+  source: "/404",
+  destination: "/404.html"
+}, {
+  source: "/401",
+  destination: "/401.html"
 }];
--- pages.js
+++ pages.js
@@ -15,4 +15,24 @@ module.exports = [
     source: "/developers",
     destination: "/developers.html",
   },
+  {
+    source: "/press",
+    destination: "/press-release.html",
+  },
+  {
+    source: "/privacy",
+    destination: "/privacy-policy.html",
+  },
+  {
+    source: "/terms",
+    destination: "/terms-of-use.html",
+  },
+  {
+    source: "/404",
+    destination: "/404.html",
+  },
+  {
+    source: "/401",
+    destination: "/401.html",
+  },
 ]
