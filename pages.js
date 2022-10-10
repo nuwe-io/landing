be9909c18bb3a094a691ce3594c2f770edcc000a
Edgar Gago Carrillo <edgargc.upc@gmail.com>
2022-10-10 10:24:14 +0200

    updated develoers as the main page and companies as the secondary

--- pages.js
+++ pages.js
@@ -1,46 +1,46 @@
 module.exports = [
   {
-    source: "/",
-    destination: "/index.html",
+    source: '/',
+    destination: '/developers.html'
   },
   {
-    source: "/404",
-    destination: "/404.html",
+    source: '/404',
+    destination: '/404.html'
   },
   {
-    source: "/404",
-    destination: "/404.html",
+    source: '/404',
+    destination: '/404.html'
   },
   {
-    source: "/developers",
-    destination: "/developers.html",
+    source: '/companies',
+    destination: '/index.html'
   },
   {
-    source: "/press",
-    destination: "/press-release.html",
+    source: '/press',
+    destination: '/press-release.html'
   },
   {
-    source: "/privacy",
-    destination: "/privacy-policy.html",
+    source: '/privacy',
+    destination: '/privacy-policy.html'
   },
   {
-    source: "/cookies",
-    destination: "/cookies.html",
+    source: '/cookies',
+    destination: '/cookies.html'
   },
   {
-    source: "/terms",
-    destination: "/terms-of-use.html",
+    source: '/terms',
+    destination: '/terms-of-use.html'
   },
   {
-    source: "/disclaimer",
-    destination: "/disclaimer.html",
+    source: '/disclaimer',
+    destination: '/disclaimer.html'
   },
   {
-    source: "/404",
-    destination: "/404.html",
+    source: '/404',
+    destination: '/404.html'
   },
   {
-    source: "/401",
-    destination: "/401.html",
-  },
+    source: '/401',
+    destination: '/401.html'
+  }
 ]
